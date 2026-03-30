c1f64ee5f53bf67ade990c21b35804c5ffe76071
ushan-Gimhan <ushangimhan@gmail.com>
2026-03-30 22:22:34 +0530

    Create initial folder structure for Sensor Service

---
+++ sensor-telemetry-service/src/main/java/com/agms/sensor_telemetry_service/SensorTelemetryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agms.sensor_telemetry_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorTelemetryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorTelemetryServiceApplication.class, args);
+	}
+
+}
---
+++ sensor-telemetry-service/src/test/java/com/agms/sensor_telemetry_service/SensorTelemetryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agms.sensor_telemetry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorTelemetryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
