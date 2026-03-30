c2e9dab397207ff64aac7f9f9963e7cfe1ca2dc9
ushan-Gimhan <ushangimhan@gmail.com>
2026-03-30 22:26:13 +0530

    Create initial folder structure for Automation Service

---
+++ automation-service/src/main/java/com/agms/automation_service/AutomationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agms.automation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationServiceApplication.class, args);
+	}
+
+}
---
+++ automation-service/src/test/java/com/agms/automation_service/AutomationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agms.automation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
