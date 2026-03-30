8d8a2d4a83036e3fad3b56f8175ed2bef68dd5f0
ushan-Gimhan <ushangimhan@gmail.com>
2026-03-30 21:05:51 +0530

    Add Zone Service with application.yml configuration for Eureka and Config Server

---
+++ zone-service/src/main/java/com/agms/zone_service/ZoneServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agms.zone_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoneServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoneServiceApplication.class, args);
+	}
+
+}
