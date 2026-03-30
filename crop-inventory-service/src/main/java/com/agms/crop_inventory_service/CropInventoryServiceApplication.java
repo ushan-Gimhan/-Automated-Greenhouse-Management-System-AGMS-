22d83c53fdd80bd6a4039c57743bc5a5e53e3f8f
ushan-Gimhan <ushangimhan@gmail.com>
2026-03-30 21:20:39 +0530

    Create initial folder structure for Crop Service (src, controller, service, repository, resources)

---
+++ crop-inventory-service/src/main/java/com/agms/crop_inventory_service/CropInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agms.crop_inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ crop-inventory-service/src/test/java/com/agms/crop_inventory_service/CropInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agms.crop_inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
