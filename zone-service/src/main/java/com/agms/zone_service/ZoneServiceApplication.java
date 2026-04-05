70e8dee8b88365b1f4a3a2f46bf00be6a18ca59e
ushan-Gimhan <ushangimhan@gmail.com>
2026-04-06 00:38:42 +0530

    update api gateway and config server and zone server application.yaml files

--- zone-service/src/main/java/com/agms/zone_service/ZoneServiceApplication.java
+++ zone-service/src/main/java/com/agms/zone_service/ZoneServiceApplication.java
@@ -2,8 +2,12 @@ package com.agms.zone_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class ZoneServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZoneServiceApplication.class, args);
