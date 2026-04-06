c86c02595ca0231c8387570b06d90a8bda892408
ushan-Gimhan <ushangimhan@gmail.com>
2026-04-06 21:12:00 +0530

    Update SensorRepository with custom query for zone filtering

--- sensor-telemetry-service/src/main/java/com/agms/sensor_telemetry_service/repository/SensorRepository.java
+++ sensor-telemetry-service/src/main/java/com/agms/sensor_telemetry_service/repository/SensorRepository.java
@@ -3,5 +3,8 @@ package com.agms.sensor_telemetry_service.repository;
 import com.agms.sensor_telemetry_service.entity.Sensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SensorRepository extends JpaRepository<Sensor, Long> {
+    List<Sensor> findByZoneId(Long zoneId);
 }
