9a2d95ac2fcca1a759e318d2192e1bb2a17cd80d
ushan-Gimhan <ushangimhan@gmail.com>
2026-04-10 23:23:19 +0530

    feat(auth-service): implement UserCredential entity and repository

---
+++ auth-service/src/main/java/com/agms/auth_service/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.agms.auth_service.repository;
+
+import com.agms.auth_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
