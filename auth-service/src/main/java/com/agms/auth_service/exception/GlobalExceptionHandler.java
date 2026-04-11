f6d825924b2c81388a836ebbb3699720c8dc5567
ushan-Gimhan <ushangimhan@gmail.com>
2026-04-11 17:16:13 +0530

    feat(auth-service):add global and custom exception classes for authentication error handling

---
+++ auth-service/src/main/java/com/agms/auth_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.agms.auth_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleNotFound(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleGeneric(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
